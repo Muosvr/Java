d50d7847083305dc2e1a1f7b5046effb55ef3036
Jason Wu <lujason2015@gmail.com>
2019-08-18 22:32:11 -0400

    Create Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math
+
+
+
+class Solution{
+    public static void main(String []argh)
+    {
+
+
+
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
+                if ( x <= (int)Math.pow(2, 31) -1 && x >= -1 * (int)Math.pow(2, 31) ){
+                    System.out.println("* int");
+                }
+
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
+
+
+
